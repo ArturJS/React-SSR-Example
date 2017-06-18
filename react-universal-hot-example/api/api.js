f4c551a8186ea29414f519b220be5639dd1146a1
ArturN <nizamutdin.art@yandex.ru>
2017-06-18 10:25:13 +0400

    Navbar redesign and changed approach for data generation for /packages

--- react-universal-hot-example/api/api.js
+++ react-universal-hot-example/api/api.js
@@ -4,6 +4,7 @@ import config from '../src/config';
 import shortid from 'shortid';
 import corsMiddleware from './middlewares/cors.middleware';
 import noCacheMiddleware from './middlewares/no-cache.middleware';
+import _ from 'lodash';
 
 const router = express.Router();
 const app = express();
@@ -15,29 +16,15 @@ app.use(noCacheMiddleware);
 
 
 router.get('/packages', (req, res) => {
-  res.json([
-    {
+  res.json(
+    _.times(20, (i) => ({
       id: shortid.generate(),
-      title: 'Package 1',
+      title: `Package ${i + 1}`,
       description: `Lorem ipsum dolor sit amet, 
       tantas expetenda dissentiunt usu ex, eum id semper 
       iisque singulis, ut vix ubique gubergren accommodare.`
-    },
-    {
-      id: shortid.generate(),
-      title: 'Package 2',
-      description: `Lorem ipsum dolor sit amet, 
-      tantas expetenda dissentiunt usu ex, eum id semper 
-      iisque singulis, ut vix ubique gubergren accommodare.`
-    },
-    {
-      id: shortid.generate(),
-      title: 'Package 3',
-      description: `Lorem ipsum dolor sit amet, 
-      tantas expetenda dissentiunt usu ex, eum id semper 
-      iisque singulis, ut vix ubique gubergren accommodare.`
-    },
-  ]);
+    }))
+  );
 });
 
 app.use('/api', router);

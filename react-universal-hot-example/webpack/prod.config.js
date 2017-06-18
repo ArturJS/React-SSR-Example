390c7715087d895719cbc8450f57680744987c38
ArturN <nizamutdin.art@yandex.ru>
2017-06-18 13:58:10 +0400

    Updated config for UglifyJsPlugin

--- react-universal-hot-example/webpack/prod.config.js
+++ react-universal-hot-example/webpack/prod.config.js
@@ -84,7 +84,15 @@ module.exports = {
     // optimizations
     new webpack.optimize.UglifyJsPlugin({
       compress: {
+        screw_ie8: true, // React doesn't support IE8
         warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
       }
     }),
     new WebpackChunkHash(),

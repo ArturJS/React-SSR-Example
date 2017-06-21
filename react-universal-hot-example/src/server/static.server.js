253d5d0c5b1fe8f5b9d228171487dc7e0815b342
Artur Nizamutdinov <artur_nizamutdinov@epam.com>
2017-06-21 17:05:24 +0400

    Added immutable caching for static assets

--- react-universal-hot-example/src/server/static.server.js
+++ react-universal-hot-example/src/server/static.server.js
@@ -5,6 +5,10 @@ import path from 'path';
 
 export const initStaticServer = (app) => {
   app.use(compression());
+  app.use((req, res, next) => {
+    res.header('Cache-Control', 'public, max-age=31536000, immutable');
+    next();
+  });
   app.use(favicon(path.join(__dirname, '../..', 'static', 'favicon.ico')));
   app.use(Express.static(path.join(__dirname, '../..', 'static')));
 };

8d7b0d93562c27b189f134ec293bf6bf25ece720
Artur Nizamutdinov <artur_nizamutdinov@epam.com>
2017-09-11 12:27:58 +0400

    Added link preload for scripts

--- src/client/helpers/Html.js
+++ src/client/helpers/Html.js
@@ -36,6 +36,9 @@ export default class Html extends Component {
 
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {Object.values(assets.javascript).map((assetPath) =>
+            <link href={assetPath} rel="preload" as="script" charSet="UTF-8"/>
+          )}
           {/* styles (will be present only in production with webpack extract text plugin) */}
           {Object.keys(assets.styles).map((style, key) =>
             <link href={assets.styles[style]} key={key} media="screen, projection"
